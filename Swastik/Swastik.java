650271d222aed3f25c09c69b8f4ab8c58519e3c1
Raviraj Gade <130790538+ravirajgade007@users.noreply.github.com>
2025-06-15 17:16:13 +0530

    Add files via upload

---
+++ Swastik/Swastik.java
@@ -0,0 +1,25 @@
+class Swastik
+{
+  public static void main(String[] args)
+   {
+       int num = 6;
+       
+       for(int i=0; i<=num; i++)
+        {
+           for(int j=0; j<=num; j++)
+            {
+               if((j == 3 && i >= 0) || (i == 3 && j >= 0)
+               || (j == 0 && i <= 2) || (i == 0 && j >= 4)
+               || (i == 6 && j <= 2) || (j == 6 && i >= 4))
+                {
+                  System.out.print("*");
+                 }
+                 else
+                     {
+			System.out.print(" ");
+                     }
+             }
+		System.out.println();
+         }
+    }
+}
