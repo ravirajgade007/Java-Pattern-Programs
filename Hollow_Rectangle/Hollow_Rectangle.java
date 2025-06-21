27f56a5bf4a5ababacaa5ed6f203443f31c44158
Raviraj Gade <130790538+ravirajgade007@users.noreply.github.com>
2025-06-21 23:18:20 +0530

    Add files via upload

---
+++ Hollow_Rectangle/Hollow_Rectangle.java
@@ -0,0 +1,24 @@
+class Hollow_Rectangle
+{
+   public static void main(String[] args)
+    {
+       int rows = 6;
+       int columns = 5;
+
+       for(int i = 1; i <= rows; i++) 
+        {
+           for(int j = 1; j<= columns; j++)
+            {
+              if(i == 1 || i == rows || j == 1 || j == columns)
+               {
+                   System.out.print("*");
+               } 
+                else
+                   {
+                      System.out.print(" ");
+                   }
+            }
+              System.out.println();
+        }
+    }
+}
